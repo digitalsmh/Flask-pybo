eff190689dc757aa3b7b54fee12ba680c30e8434
송명희 <smheedigital@gmail.com>
2023-09-12 15:21:52 +0900

    2023.9.12-3 변경

---
+++ Init_DB.sh
@@ -0,0 +1,17 @@
+cd ~/projects/myproject
+
+# 데이터베이스 파일, 디렉토리 삭제 
+del pybo.db   
+rm -rf migrations
+
+# 데이터베이스 초기화   
+flask db init    
+
+# 리비전 파일 생성    
+flask db migrate 
+
+# 가장 최근 버전(리비전 파일 실행) DB upgrade   
+flask db upgrade 
+
+# 이전 버전으로 DB downgrade   
+# flask db downgrade 
