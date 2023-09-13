ed4bc08009056b79aa62ee0e802bc948fd2d3023
송명희 <smheedigital@gmail.com>
2023-09-13 17:33:11 +0900

    로깅"

--- config/production.py
+++ config/production.py
@@ -1,7 +1,31 @@
 # [파일명: projects/myproject/config/production.py]
-
+from logging.config import dictConfig
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'VJ!sA3\xfe\xf5\xc1\xb5\xfec\xc0Y\xd4\x19'
+
+# logging : 파이썬의 기본 logging 모듈
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
