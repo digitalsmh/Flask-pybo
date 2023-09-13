e024627c0b11b2670aa8bc3e400ed5c507b36412
송명희 <smheedigital@gmail.com>
2023-09-13 17:08:15 +0900

    로깅

--- win_dev_project.cmd
+++ win_dev_project.cmd
@@ -1,11 +1,6 @@
 @REM Windws 개발환경
-@echo off
 cd D:\3.Python\16_Flask_deshboard_project\pybo_project\myproject>
 set FLASK_APP=pybo
 set FLASK_DEBUG=true
-
-@REM 구성파일 환경 변수 설정 : config\development.py
-set APP_CONFIG_FILE=
-D:\3.Python\16_Flask_deshboard_project\pybo_project\myproject\config\development.py
-
-@REM c:/venvs/myproject/scripts/activate
+set APP_CONFIG_FILE=D:\3.Python\16_Flask_deshboard_project\pybo_project\myproject\config\development.py
+flask run --host=0.0.0.0
